b953eba868974ea584805c8c3bda76c1943734b5
VjacheslavRogov <slava.eogov.98@gmail.com>
2023-02-28 13:18:19 +0300

    added PrintArray, IncorreckValue

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+void IncorrectValue()
+{
+    Console.WriteLine("Введено некорректное значение.");
+    Environment.Exit(0);
+}
+
+int UserInput()
+{
+    if (!int.TryParse(Console.ReadLine(), out int temp)) IncorrectValue();
+    return temp;
+}
